9b1b759aad98a3e367286c96e739ab647cb32022
Shailly Yadav <202251123@iiitvadodara.ac.in>
2024-08-01 18:10:35 +0530

    data validation completed

--- src/textSummarizer/components/__init__.py
+++ src/textSummarizer/components/__init__.py
@@ -2,9 +2,11 @@ import os
 import urllib.request as request
 import zipfile
 from textSummarizer.entity import DataIngestionConfig
+from textSummarizer.entity import DataValidationConfig
 from textSummarizer.logging import logger
 from textSummarizer.utils.common import get_size
 from pathlib import Path
+
 
 
 class DataIngestion:
@@ -33,3 +35,32 @@ class DataIngestion:
         os.makedirs(unzip_path, exist_ok=True)
         with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
             zip_ref.extractall(unzip_path)
+
+
+
+class DataValidation:
+    def __init__(self, config: DataValidationConfig):
+        self.config = config
+
+
+    
+    def validate_all_files_exist(self)-> bool:
+        try:
+            validation_status = None
+
+            all_files = os.listdir(os.path.join("artifacts","data_ingestion","samsum_dataset"))
+
+            for file in all_files:
+                if file not in self.config.ALL_REQUIRED_FILES:
+                    validation_status = False
+                    with open(self.config.STATUS_FILE, 'w') as f:
+                        f.write(f"Validation status: {validation_status}")
+                else:
+                    validation_status = True
+                    with open(self.config.STATUS_FILE, 'w') as f:
+                        f.write(f"Validation status: {validation_status}")
+
+            return validation_status
+        
+        except Exception as e:
+            raise e
